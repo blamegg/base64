75b65f0b49a686ccb0348eb87bdc22752d87c8ed
shivang <shivang.ramola.hp@gmail.com>
2025-01-23 16:03:32 +0530

    basic setup

--- src/components/FileToBase64Converter/index.jsx
+++ src/components/FileToBase64Converter/index.jsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 const FileToBase64Converter = () => {
   const [base64, setBase64] = useState("");
   const [error, setError] = useState("");
-  const [copied, setCopied] = useState(false);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -14,7 +13,6 @@ const FileToBase64Converter = () => {
     reader.onloadend = () => {
       setBase64(reader.result);
       setError("");
-      setCopied(false); // Reset the copied state
     };
 
     reader.onerror = () => {
@@ -22,12 +20,6 @@ const FileToBase64Converter = () => {
     };
 
     reader.readAsDataURL(file);
-  };
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(base64).then(() => {
-      setCopied(true);
-    });
   };
 
   return (
@@ -45,17 +37,7 @@ const FileToBase64Converter = () => {
             readOnly
             style={{ width: "100%", marginTop: "10px" }}
           />
-          <div style={{ marginTop: "10px" }}>
-            <button onClick={handleCopy}>
-              {copied ? "Copied!" : "Copy to Clipboard"}
-            </button>
-          </div>
         </div>
-      )}
-      {base64 && (
-        <>
-          <img src={base64} alt="" className="h-28 w-28" />
-        </>
       )}
     </div>
   );
